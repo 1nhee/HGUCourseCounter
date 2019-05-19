1e913ceb088968be426991fc38f94a5fd979d418
1nhee <21800025@handong.edu>
2019-05-19 18:28:10 +0900

    utils class

---
+++ src/main/java/edu/handong/analysis/utils/Utils.java
@@ -0,0 +1,11 @@
+package edu.handong.analysis.utils;
+
+public class Utils {
+	public static ArrayList<String> getLines(String file,boolean removeHeader){
+		//getLines method reads csv file from the given file path and returns ArrayList<String> object. One element has a string of one line. The second argument is the option to avoid including first line to ArrayList when true.
+	}
+	public static void writeAFile(ArrayList<String> lines, String targetFileName) {
+		//writeAFile saves the result information in the ArrayList to a file in the second argument. If a file does not exist in the directory, FileNotFoundException may occur, to prevent, a logic to create directory is required. There is a hint at TIPS below.
+	}
+
+}
