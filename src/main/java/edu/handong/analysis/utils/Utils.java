1c8dad714d88c83ec3ab6cdd36b4b45a1c89d49d
1nhee <21800025@handong.edu>
2019-05-19 18:35:41 +0900

    utils

--- src/main/java/edu/handong/analysis/utils/Utils.java
+++ src/main/java/edu/handong/analysis/utils/Utils.java
@@ -1,7 +1,10 @@
 package edu.handong.analysis.utils;
 
+import java.util.ArrayList;
+
 public class Utils {
 	public static ArrayList<String> getLines(String file,boolean removeHeader){
+		return null;
 		//getLines method reads csv file from the given file path and returns ArrayList<String> object. One element has a string of one line. The second argument is the option to avoid including first line to ArrayList when true.
 	}
 	public static void writeAFile(ArrayList<String> lines, String targetFileName) {
