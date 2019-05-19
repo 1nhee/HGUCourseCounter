d6768b88d4235f1022cc31a5bce179b04a4fedf0
1nhee <21800025@handong.edu>
2019-05-19 18:28:29 +0900

    student.java

--- src/main/java/edu/handong/analysis/datamodel/Student.java
+++ src/main/java/edu/handong/analysis/datamodel/Student.java
@@ -1,5 +1,23 @@
 package edu.handong.analysis.datamodel;
 
 public class Student {
-
+	
+	private String studentId;
+	private ArrayList<Course> coursesTaken; // List of courses student has taken
+	private HashMap<String,Integer> semestersByYearAndSemester; 
+	                                                         // key: Year-Semester
+	                                                         // e.g., 2003-1, 
+	public Student(String studentId) // constructor
+	public void addCourse(Course newRecord) {
+		///add a Course instance created while reading line to the CourseTaken ArrayList in the Student instance.
+	}
+	public HashMap<String,Integer> getSemestersByYearAndSemester(){
+		//creates a hashmap to store the student’s sequential semester information by using strong year and semester information.
+	}
+	public int getNumCourseInNthSementer(int semester) {
+		//returns the number of subjects in the semester if you enter the sequential semester number.
+		//HashMap에 key로 학기 수를 입력하면 그 학기 때 들었던  과목의 수를 return
+	}
+	/* Add getter and setter for the field if needed*/
+	
 }
