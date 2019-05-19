185530b68fe0848e9efdf36c3bb7f542d5409010
1nhee <21800025@handong.edu>
2019-05-19 18:28:46 +0900

    NotEnoughArgumentException

--- src/main/java/edu/handong/analysis/utils/NotEnoughArgumentException.java
+++ src/main/java/edu/handong/analysis/utils/NotEnoughArgumentException.java
@@ -1,5 +1,11 @@
-package ii;
+package edu.handong.analysis.utils;
 
 public class NotEnoughArgumentException {
-
+	//With Customized Exception, you only need to extend the Exception and implement two constructors as you learned in the class. Refer to Exception message below.
+	public NotEnoughArgumentException(){
+		
+	}
+	public NotEnoughArgumentException(String message){
+		
+	}
 }
