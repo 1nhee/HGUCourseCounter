e903321add0df1e96ad2945cf6380b3325c0e7ae
1nhee <21800025@handong.edu>
2019-05-20 23:06:36 +0900

    coomit

--- src/main/java/edu/handong/analysis/utils/Utils.java
+++ src/main/java/edu/handong/analysis/utils/Utils.java
@@ -1,14 +1,56 @@
 package edu.handong.analysis.utils;
 
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
+import java.util.Scanner;
+import edu.handong.analysis.datamodel.*;
 
 public class Utils {
-	public static ArrayList<String> getLines(String file,boolean removeHeader){
+	public static ArrayList<String> getLines(String file, boolean removeHeader){
+		
+		ArrayList<Student> studentArray = new ArrayList<Student>();
+		
+		String fileName = file;
+		try{
+			// Skip the header line by reading and ignoring it
+			Scanner inputStream = new Scanner(new File(fileName)); 
+			String line = inputStream.nextLine();
+			
+			double total = 0;
+			// Read the rest of the file line by line
+			while (inputStream.hasNextLine())
+			{
+				
+				line = inputStream.nextLine();
+
+				// Turn the string into an array of strings
+				String studentNumber = line.split(",")[0];
+				String studentNumber = line.split(",")[1];
+				
+				
+				// Extract each item into an appropriate variable
+				
+				int quantity = Integer.parseInt(ary[1]);
+				double price = Double.parseDouble(ary[2]);
+				String description = ary[3];
+				
+				// Output item
+				
+				// Compute total
+				total += quantity * price;
+			}
+			System.out.println("Total sales: " + total);
+			inputStream.close( );
+		}
+		catch(FileNotFoundException e) {
+			System.out.println("Cannot find file " + fileName);
+		}
 		return null;
-		//getLines method reads csv file from the given file path and returns ArrayList<String> object. One element has a string of one line. The second argument is the option to avoid including first line to ArrayList when true.
-	}
-	public static void writeAFile(ArrayList<String> lines, String targetFileName) {
-		//writeAFile saves the result information in the ArrayList to a file in the second argument. If a file does not exist in the directory, FileNotFoundException may occur, to prevent, a logic to create directory is required. There is a hint at TIPS below.
 	}
 
 }
