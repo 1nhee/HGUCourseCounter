4d1d40d7d4398909e9bcaa39ddee931f3ca916a7
1nhee <21800025@handong.edu>
2019-05-21 14:29:01 +0900

    write string

--- src/main/java/edu/handong/analysis/utils/NotEnoughArgumentException.java
+++ src/main/java/edu/handong/analysis/utils/NotEnoughArgumentException.java
@@ -1,11 +1,15 @@
 package edu.handong.analysis.utils;
 
-public class NotEnoughArgumentException {
-	//With Customized Exception, you only need to extend the Exception and implement two constructors as you learned in the class. Refer to Exception message below.
+public class NotEnoughArgumentException extends Exception{
+	/With Customized Exception, you only need to extend the Exception 
+	and implement two constructors as you learned in the class. 
+	Refer to Exception message below.*/
 	public NotEnoughArgumentException(){
-		
+		String exceptionString ="The file path does not exist. Please check your CLI argument!";		
+		System.out.println(exceptionString);
 	}
 	public NotEnoughArgumentException(String message){
-		
+		String CLIString = "No CLI argument Exception! Please put a file path.";
+		System.out.println(CLIString);
 	}
 }
