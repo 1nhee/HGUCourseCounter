20413b9b3bf3335df362c08fc72bc998c446a25f
1nhee <21800025@handong.edu>
2019-05-22 12:42:55 +0900

    student

--- src/main/java/edu/handong/analysis/datamodel/Student.java
+++ src/main/java/edu/handong/analysis/datamodel/Student.java
@@ -26,19 +26,41 @@ public class Student {
 		/*creates a hashmap to store the student’s sequential semester information 
 		by using string year and semester information.*/
 		
-		if(coursesTaken.get(index)
-			
-		semestersByYearAndSemester.put(key, value)
+		int startYear = coursesTaken.get(0).getYearTaken();
+		int startSemester = coursesTaken.get(0).getSemesterCourseTaken();
+		int count = 0;
 		
-		return semestersByYearAndSemester;
+		for(Course courseToCheck:this.coursesTaken) {
+
+			int currYear = courseToCheck.getYearTaken();
+			int currSemester = courseToCheck.getSemesterCourseTaken();
+			String currYearAndSemester = currYear + "-" + currSemester;
+			
+			//first semester
+			if((currYear == startYear) && (currSemester != startSemester)) {
+				semestersByYearAndSemester.put(currYearAndSemester, 1);
+				count = 1;
+			}
+			
+			//second semester
+			if(count == 1) {
+				semestersByYearAndSemester.put(currYearAndSemester, 2);
+				count++;
+			}
+			
+			if(currYear != startYear)
+				semestersByYearAndSemester.put(currYearAndSemester, );
+		}
+		
+		
+		return this.semestersByYearAndSemester;
 	}
-	public int getNumCourseInNthSementer(int semester) {
+	public int getNumCourseInNthSemester(int semester) {
 		/*returns the number of subjects in the semester 
 		 * if you enter the sequential semester number.
 		HashMap에 key로 학기 수를 입력하면 그 학기 때 들었던  과목의 수를 return*/
-		
-		Integer getNthSemester = new Integer(semester);
-		int numOfCourseTaken = this.semestersByYearAndSemester.get(getNthSemester);
+	
+		if(coursesTaken.)
 		
 		return numOfCourseTaken;
 		
